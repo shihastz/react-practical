84e639c270ffb0d1f8fc05bd95b501340a1286b5
shihastz <shihast0503@gmail.com>
2020-05-22 21:28:48 +0530

    Updates

---
+++ precache-manifest.16173cc374d43c56a43d8c155b9d2ba0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddc737e12c2f62ab83a2ae164d17266d",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "2aeb04e82056e6dc4f73",
+    "url": "/react-app/static/css/2.244f50d0.chunk.css"
+  },
+  {
+    "revision": "9437387075b4cef87a72",
+    "url": "/react-app/static/css/main.d80d4f7a.chunk.css"
+  },
+  {
+    "revision": "2aeb04e82056e6dc4f73",
+    "url": "/react-app/static/js/2.1f7488ed.chunk.js"
+  },
+  {
+    "revision": "7ec7a85a6c7312407b46dda354a04a82",
+    "url": "/react-app/static/js/2.1f7488ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9437387075b4cef87a72",
+    "url": "/react-app/static/js/main.8d85d322.chunk.js"
+  },
+  {
+    "revision": "1dc033653be254afbc4b",
+    "url": "/react-app/static/js/runtime-main.068afa05.js"
+  }
+]);
